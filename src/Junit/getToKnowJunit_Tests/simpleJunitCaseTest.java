bbc481f0f7386b0db81fbe03c7c687d548f68452
Joel <Joel@DESKTOP-S01C55B>
2019-01-25 17:09:52 +0100

    test

--- src/Junit/getToKnowJunit_Tests/simpleJunitCaseTest.java
+++ src/Junit/getToKnowJunit_Tests/simpleJunitCaseTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 class simpleJunitCaseTest {
 	@Test
 	void testFindMax() {
+		//
 		assertEquals(4,getToKnowJunit.simpleJunitCase.findMax(new int[]{1,3,4,2}));  
 	    assertEquals(-1,getToKnowJunit.simpleJunitCase.findMax(new int[]{-12,-1,-3,-4,-2}));  
 	}

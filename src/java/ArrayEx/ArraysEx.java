f8ce060f47e131a416ab9572d661c58b1e72478b
Joel <Joel@DESKTOP-S01C55B>
2019-01-26 17:23:00 +0100

    generated comments for my arraysExamples class.

--- src/java/ArrayEx/ArraysEx.java
+++ src/java/ArrayEx/ArraysEx.java
@@ -4,28 +4,45 @@ import java.util.ArrayList;
 public class ArraysEx {
     
     public static void main(String[] args) {
-        //reorderArray();
-        stringArrayAddNull();
+        //run the following function
+//    	stringArrayAddNull();
+    	String[] myArray = {"Joe", null, null, "Fred"};
+    	String[] reorderedArray = reorderArray(myArray);  
     }
-    public static String stringArrayAddNull() {
-        String[] myArray = {"Apple", "Banana"};
+    /**
+     * In this example I checked, if I can add the value NULL to a string
+     * array. Which indeed works.
+     * @author Joel
+     */
+    public static void stringArrayAddNull() {
+    	//initialize the array
+        String[] myArray = {"Super", "Example", "Overwrite with null"};
         System.out.println(Arrays.toString(myArray));
-        myArray[1] = null;
+        myArray[2] = null;
         System.out.println(Arrays.toString(myArray));
-        return "Ok";
     }
-    public static String reorderArray() {
-        String[] myArray = {"joeqwerlie", null, null, "Durian"};
+    /**
+     * I have a string array of four, which could include NULL values.
+     * This function moves the NULL values to the end of the array.
+     * @param myArray {String[]} - an array, where the NULL values
+     * should be at the end.
+     * @return string array, with the size of four. And if it contains
+     * NULL values, they are at the end of the array.
+     */
+    public static String[] reorderArray(String[] myArray) {
+    	//create and arraylist
         ArrayList<String> regroupArray = new ArrayList<String>();
         System.out.println(Arrays.toString(myArray));
+        //each arrayValue
         for(String i: myArray) {
-            //System.out.println(i);
+        	//if not null, add to the arraylist
             if(i != null) {
                 regroupArray.add(i);
             }
         }
+        //transform arraylist to string array with the size of four
         myArray = regroupArray.toArray(new String[4]);
         System.out.println(Arrays.toString(myArray));
-        return "OK";
+        return myArray;
     }
 }

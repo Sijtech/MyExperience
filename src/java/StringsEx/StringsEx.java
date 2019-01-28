f47bc23094a49b596e4630f8bac0e0aab2082941
sijtech <joesiewerdt@gmail.com>
2019-01-28 13:38:27 +0100

    verified that a string with the value "" is empty

---
+++ src/java/StringsEx/StringsEx.java
@@ -0,0 +1,17 @@
+package StringsEx;
+
+public class StringsEx {
+	public static void main(String[] args) {
+        //run the following function
+		isStringEmpty();
+    }
+    /**
+     * Verified, if the value "" of a string means it is empty.
+     * @author Joel
+     */
+    public static void isStringEmpty() {
+    	String isItEmpty = "";
+        System.out.println(isItEmpty.isEmpty());
+    }
+
+}

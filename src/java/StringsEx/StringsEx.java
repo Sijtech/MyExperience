9ed94281648171cbb91c2bda57c9fac735f3a9af
sijtech <joesiewerdt@gmail.com>
2019-02-07 14:52:30 +0100

    example file for strings

--- src/java/StringsEx/StringsEx.java
+++ src/java/StringsEx/StringsEx.java
@@ -3,7 +3,7 @@ package StringsEx;
 public class StringsEx {
 	public static void main(String[] args) {
         //run the following function
-		isStringEmpty();
+		addToStringBeginning();
     }
     /**
      * Verified, if the value "" of a string means it is empty.
@@ -12,6 +12,15 @@ public class StringsEx {
     public static void isStringEmpty() {
     	String isItEmpty = "";
         System.out.println(isItEmpty.isEmpty());
+    }
+    /**
+     * add to the beginning of a string some text.
+     * @author Joel
+     */
+    public static void addToStringBeginning() {
+    	String toExtend = "is a test";
+    	toExtend = "This "+toExtend;
+        System.out.println(toExtend);
     }
 
 }
